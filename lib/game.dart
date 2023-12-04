5db002d4822e5abe11dc78e5e647db935e311539
artem <art.zavtur@gmail.com>
2023-12-04 14:19:46 +0200

    clean up

---
+++ lib/game.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatelessWidget {
+  const Game({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Random Puzzle'),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center();
+  }
+}
